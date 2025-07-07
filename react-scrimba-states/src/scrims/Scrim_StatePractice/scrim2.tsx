d9d2448c583a1dc041c4c2bf94b7aa8b7b54ce03
cdricn <cdricncdricn@gmail.com>
2025-07-07 22:17:19 +0800

    Scrim 3

---
+++ react-scrimba-states/src/scrims/Scrim_StatePractice/scrim2.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './scrim2.css'
+
+export default function App() {
+    /**
+     * Challenge: 
+     * Create state to track our count value (initial value is 0)
+     * Don't forget to replace the hard-coded "0" with your new state
+     */
+    const [count, setCount] = useState(0)
+
+    function add() {
+      setCount(prevCount => prevCount + 1);
+    }
+    function minus() {
+      setCount(prevCount => prevCount - 1);
+    }
+
+    return (
+        <main className="container">
+            <h1>How many times will Bob say "state" in this section?</h1>
+            <div className="counter">
+                <button onClick={minus} className="minus" aria-label="Decrease count">–</button>
+                <h2 className="count">{count}</h2>
+                <button onClick={add} className="plus" aria-label="Increase count">+</button>
+            </div>
+        </main>
+    )
+}
