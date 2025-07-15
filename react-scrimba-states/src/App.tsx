f095a56cac6e1660aa4820acf7c7112e6a99d2a5
cdricn <cdricncdricn@gmail.com>
2025-07-15 18:00:55 +0800

    Course update

--- react-scrimba-states/src/App.tsx
+++ react-scrimba-states/src/App.tsx
@@ -1,17 +1,15 @@
 import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
+import Scrim6_2 from './scrims/Scrim6_ConditionalRender/scrim6_2'
 // import Scrim5 from './scrims/Scrim5_Forms/scrim5'
-// import Scrim4 from './scrims/Scrim4_StateObjects/scrim4'
-// import Scrim1 from './scrims/Scrim_EventListener/scrim1'
-import Scrim2 from './scrims/Scrim2_StatePractice/scrim2'
 
 export default function App() {
 
   return (
     <>
       <Header />
-      <Scrim2 />
+      <Scrim6_2/>
     </>
   )
 }
